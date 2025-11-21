fcbf93d2e786d4dfd7ce8742bca12eaa0c0e1e78
Adeshmeena72 <adeshmeena72@gmail.com>
2025-11-21 15:12:09 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,9 +37,7 @@ export default function RootLayout({
         <Navbar/>
         <TopToolsBar/>
         <PulseHeaderBar />
-        {/* <NewPairsSection /> */}
         <ThreeColumnsBoard />
-        {/* <CryptoDashboardLucide/> */}
         <BottomBar/>
       </body>
     </html>
